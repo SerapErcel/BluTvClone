e27cda0321e451b297e0b9aa75ad13cdda68cc15
Serap <ercelserap@gmail.com>
2022-10-18 20:42:55 +0300

    Add bottom navigation bar

--- app/src/main/java/com/serapercel/blutvclone/MainActivity.kt
+++ app/src/main/java/com/serapercel/blutvclone/MainActivity.kt
@@ -2,10 +2,23 @@ package com.serapercel.blutvclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.serapercel.blutvclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        binding.bottomNav.itemIconTintList = null
+
+        NavigationUI.setupWithNavController(binding.bottomNav, navHostFragment.navController)
     }
 }
